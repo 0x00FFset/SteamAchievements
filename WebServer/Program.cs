96dd58459d1ab3604355a93b18274151e47a614e
pierrot <pierrot@cs2tools.info>
2024-02-11 23:44:45 +0000

    Add web server files and static assets

---
+++ WebServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using System.IO;
+using System.Reflection;
+
+
+namespace WebServer;
+
+public class Program
+{
+
+    public static void Main(string[] args) => StartWebServer(args);
+
+    public static void StartWebServer(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.UseStaticFiles();
+
+        // Render index.html
+        app.MapGet("/", async context =>
+        {
+            await context.Response.SendFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"));
+        });
+
+        app.Run();
+    }
+}
