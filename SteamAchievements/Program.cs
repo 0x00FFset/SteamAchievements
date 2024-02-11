830d8d22f29d0adae86f0f552b71bad510847cdf
pierrot <pierrot@cs2tools.info>
2024-02-11 23:44:51 +0000

    Add SteamAchievements project and solution files

---
+++ SteamAchievements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Quic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SteamKit2;
+using WebServer;
+
+namespace SteamAchievements;
+
+internal static class Program
+{
+
+    private static void Main(string[] args)
+    {
+        WebServer.Program.Main(args);
+    }
+
+}
