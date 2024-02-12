092fad019612d2697cf9b3792543b9e70ec9d4db
pierrot <pierrot@offset.cx>
2024-02-12 20:13:38 +0000

    Add new callback classes for AppDataChanged and UserStatsReceived

---
+++ SteamAchievements.API/Callbacks/AppDataChanged.cs
@@ -0,0 +1,14 @@
+﻿namespace SteamAchievements.API.Callbacks;
+
+public class AppDataChanged : Callback<Types.AppDataChanged>
+{
+    public override int Id
+    {
+        get { return 1001; }
+    }
+
+    public override bool IsServer
+    {
+        get { return false; }
+    }
+}
---
+++ SteamAchievements.API/Callbacks/UserStatsReceived.cs
@@ -0,0 +1,14 @@
+﻿namespace SteamAchievements.API.Callbacks;
+
+public class UserStatsReceived : Callback<Types.UserStatsReceived>
+{
+    public override int Id
+    {
+        get { return 1101; }
+    }
+
+    public override bool IsServer
+    {
+        get { return false; }
+    }
+}
